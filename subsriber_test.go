b33d7ebb09f24e1cf96abe6621a5abda203937a2
Brett Allred <brettallred@gmail.com>
2016-10-26 21:35:06 -0600

    Fix GoLint, Adds method for PrefixQueueInDev

---
+++ subsriber_test.go
@@ -0,0 +1,27 @@
+package rabbit_test
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"testing"
+
+	"github.com/brettallred/rabbit-go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrefixQueueInDev(t *testing.T) {
+	var subscriber = rabbit.Subscriber{
+		Concurrency: 5,
+		Durable:     true,
+		Exchange:    "events",
+		Queue:       "test.sample.event.created",
+		RoutingKey:  "sample.event.created",
+	}
+
+	os.Setenv("APP_ENV", "development")
+	subscriber.PrefixQueueInDev()
+	userData, _ := user.Current()
+
+	assert.Equal(t, subscriber.Queue, fmt.Sprintf("%s_test.sample.event.created", userData.Username))
+}
