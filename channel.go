bb87fb53047b0c4eae273ddb3b4eb8de367d9548
Brett Allred <brettallred@gmail.com>
2016-11-16 15:03:27 -0700

    improved refactor

--- channel.go
+++ channel.go
@@ -6,19 +6,20 @@ import (
 
 func createAutoClosingChannel(conn *amqp.Connection) (*amqp.Channel, error) {
 	channel, err := conn.Channel()
+
 	if err != nil {
 		return channel, err
 	}
 
-	errorChannel := make(chan *amqp.Error, 1)
-	channel.NotifyClose(errorChannel)
-
-	go closeConnectionOnChannelNotifyClose(errorChannel)
+	go closeConnectionOnChannelNotifyClose(channel)
 
 	return channel, nil
 }
 
-func closeConnectionOnChannelNotifyClose(errorChannel chan *amqp.Error) {
+func closeConnectionOnChannelNotifyClose(channel *amqp.Channel) {
+	errorChannel := make(chan *amqp.Error, 1)
+	channel.NotifyClose(errorChannel)
+
 	select {
 	case <-errorChannel:
 		lock.Lock()
